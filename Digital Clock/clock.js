3c54eb7be905c21a3473d172d15d469498a62ce7
Ankit Patel <65730409+cuteankit@users.noreply.github.com>
2024-05-16 16:14:25 +0530

    Delete Digital Clock directory

--- Digital Clock/clock.js
+++
@@ -1,9 +0,0 @@
-const clock = document.getElementById('clock')
-
-
-// console.log(clock1.toLocaleTimeString());
- 
-setInterval(function () {
-    const date = new Date()
-    clock.innerHTML = date.toLocaleTimeString();
-}, 1000)
